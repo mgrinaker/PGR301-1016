3f3de5eb70f92094cf363402223df560b74cc135
Martine Grinaker <77102703+mgrinaker@users.noreply.github.com>
2022-11-29 14:44:18 +0100

    Want terraform to fail

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -6,25 +6,6 @@ resource "aws_cloudwatch_dashboard" "main" {
   "widgets": [
     {
       "type": "metric",
-      "x": 0,
-      "y": 0,
-      "width": 12,
-      "height": 6,
-      "properties": {
-        "metrics": [
-          [
-            "${var.candidate_id}",
-            "account_count.value"
-          ]
-        ],
-        "period": 300,
-        "stat": "Maximum",
-        "region": "eu-west-1",
-        "title": "Total number of accounts"
-      }
-    },
-    {
-      "type": "metric",
       "x": 5,
       "y": 5,
       "width": 12,
